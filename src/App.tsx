a7c292be73cf3ef62b7682034812382f323764ce
berkantkazangirler <berkantkazangirler@gmail.com>
2024-09-23 12:47:29 +0300

    general commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <span className="text-red-400">asd</span>
+    </>
+  );
+}
+
+export default App;
